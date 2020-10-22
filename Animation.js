536bc77685f80fc895993f0bae6f4331218b0ef6
DronPlayz <73122251+DronPlayz@users.noreply.github.com>
2020-10-22 22:19:05 +0530

    Update Animation.js

--- Animation.js
+++ Animation.js
@@ -1,1 +1,12 @@
 
+function setup() { 
+    createCanvas(400, 400); 
+} 
+  
+function draw() { 
+    background(220); 
+    fill('yellow'); 
+      
+    // An ellipse at 150, 150 with radius 280 
+    ellipse(150, 150, 280, 180)  
+}  
